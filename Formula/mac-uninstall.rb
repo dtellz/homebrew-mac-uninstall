9bff0223a87ff46f922f6c73b3fb0f1c4a0ab77f
Diego <dtellez95@gmail.com>
2024-03-05 23:45:44 +0000

    add release and shasum

--- Formula/mac-uninstall.rb
+++ Formula/mac-uninstall.rb
@@ -1,8 +1,8 @@
 class MacUninstall < Formula
     desc "CLI tool to uninstall macOS apps by deleting related files and the application itself"
     homepage "https://github.com/dtellz/mac-uninstall"
-    url "https://github.com/dtellz/mac-uninstall/archive/refs/tags/v1.0.0.tar.gz" # Update this URL
-    sha256 "" # Update with the actual SHA-256 checksum of your tar.gz file
+    url "https://github.com/dtellz/mac-uninstall/archive/refs/tags/v1.0.0.tar.gz"
+    sha256 "3571ad015f14b2f3bfdc2f80e3abec609e145faedea88b6b275827cacba45c2e"
     license "MIT"
   
     def install
