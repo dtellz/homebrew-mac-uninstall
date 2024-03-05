26cb303b27ada723c074db7fbfddcedea95379d4
Diego <dtellez95@gmail.com>
2024-03-05 23:27:11 +0000

    add homebrew tap

---
+++ Formula/mac-uninstall.rb
@@ -0,0 +1,16 @@
+class MacUninstall < Formula
+    desc "CLI tool to uninstall macOS apps by deleting related files and the application itself"
+    homepage "https://github.com/username/mac-uninstall"
+    url "https://github.com/username/mac-uninstall/archive/refs/tags/v1.0.0.tar.gz" # Update this URL
+    sha256 "" # Update with the actual SHA-256 checksum of your tar.gz file
+    license "MIT"
+  
+    def install
+      bin.install "bin/mac-uninstall"
+    end
+  
+    test do
+      system "#{bin}/mac-uninstall", "--version"
+    end
+  end
+  
