49fcdce3373e3a2678fe4f40669f0c71a5479ecc
Diego <dtellez95@gmail.com>
2024-03-06 00:28:41 +0000

    bump tag version and sha

--- Formula/mac-uninstall.rb
+++ Formula/mac-uninstall.rb
@@ -1,8 +1,8 @@
 class MacUninstall < Formula
     desc "CLI tool to uninstall macOS apps by deleting related files and the application itself"
     homepage "https://github.com/dtellz/mac-uninstall"
-    url "https://github.com/dtellz/mac-uninstall/archive/refs/tags/v1.0.0.tar.gz"
-    sha256 "91013c2ea828ce1045aa7aed206dc46c102b0da33d4b4a18991230586720c76f"
+    url "https://github.com/dtellz/mac-uninstall/archive/refs/tags/v1.0.1.tar.gz"
+    sha256 "4a807135fa2cde4680f80053e723e0def1c46e659ddd5bebaae5eaf649c731e2"
     license "MIT"
   
     def install
