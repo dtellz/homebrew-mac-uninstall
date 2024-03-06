01b958eb4ac121de1ed7639322612990cfe15901
Diego <dtellez95@gmail.com>
2024-03-06 00:09:10 +0000

    update with renamed repo sha

--- Formula/mac-uninstall.rb
+++ Formula/mac-uninstall.rb
@@ -2,7 +2,7 @@ class MacUninstall < Formula
     desc "CLI tool to uninstall macOS apps by deleting related files and the application itself"
     homepage "https://github.com/dtellz/mac-uninstall"
     url "https://github.com/dtellz/mac-uninstall/archive/refs/tags/v1.0.0.tar.gz"
-    sha256 "3571ad015f14b2f3bfdc2f80e3abec609e145faedea88b6b275827cacba45c2e"
+    sha256 "91013c2ea828ce1045aa7aed206dc46c102b0da33d4b4a18991230586720c76f"
     license "MIT"
   
     def install
